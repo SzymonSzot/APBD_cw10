e45e5fca558f70a8efc649d7773c82c3b82b1b71
Szymon Szot <s33692@pjwstk.edu.pl>
2026-05-14 17:37:17 +0200

    Controller init

---
+++  APBD10/Controllers/PcComponentController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APBD10.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PcComponentController : ControllerBase
+    {
+    }
+}
