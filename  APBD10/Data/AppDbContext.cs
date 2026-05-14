84b785adb46c28f1118bcae77954f52345f07638
Szymon Szot <s33692@pjwstk.edu.pl>
2026-05-14 15:44:23 +0200

    Db connection

---  APBD10/Data/AppDbContext.cs
+++  APBD10/Data/AppDbContext.cs
@@ -5,13 +5,17 @@ namespace APBD10.Data;
 
 public class AppDbContext : DbContext
 {
-    protected AppDbContext(DbContextOptions options) : base(options) { }
-
-    public AppDbContext() { }
+    protected AppDbContext() { }
+    public AppDbContext(DbContextOptions options) : base(options) { }
     
     public DbSet<Component> Components { get; set; }
     public DbSet<Pc> Pcs { get; set; }
     public DbSet<PcComponent> PcComponents { get; set; }
     public DbSet<ComponentType> ComponentTypes { get; set; }
     public DbSet<ComponentManufacturer> ComponentManufacturers { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
 }
