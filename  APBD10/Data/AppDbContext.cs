ceb246492c55456693e9dad2f58d8a8cd5f70ea1
Szymon Szot <s33692@pjwstk.edu.pl>
2026-05-14 13:40:40 +0200

    dbContext base

---  APBD10/Data/AppDbContext.cs
+++  APBD10/Data/AppDbContext.cs
@@ -1,7 +1,17 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using APBD10.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace APBD10.Data;
 
 public class AppDbContext : DbContext
 {
+    protected AppDbContext(DbContextOptions options) : base(options) { }
+
+    public AppDbContext() { }
+    
+    public DbSet<Component> Components { get; set; }
+    public DbSet<Pc> Pcs { get; set; }
+    public DbSet<PcComponent> PcComponents { get; set; }
+    public DbSet<ComponentType> ComponentTypes { get; set; }
+    public DbSet<ComponentManufacturer> ComponentManufacturers { get; set; }
 }
